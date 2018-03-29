30d4af88349891851aca1f162f9b136efdecbdb4
SportApps <32810117+AlexanderStepanenko@users.noreply.github.com>
2018-03-29 23:13:27 +0300

    innitial commit

---
+++ app/src/main/java/company/sportapps/payment/Payment.java
@@ -0,0 +1,34 @@
+package company.sportapps.payment;
+
+public class Payment {
+
+    private String name;
+    private float price;
+
+    Payment(String name, float price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public  String toString(){
+        return name + " " + String.valueOf(price);
+    }
+}
+
---
+++ app/src/main/java/company/sportapps/payment/View/NewPaymentRequest1.java
@@ -0,0 +1,15 @@
+package company.sportapps.payment.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import company.sportapps.payment.R;
+
+public class NewPaymentRequest1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_payment_request1);
+    }
+}
